46422e37ba4a521e871f2602846b732c0df4fcb7
DunkaN <lguarnizl@gmail.com>
2022-09-04 11:28:54 -0500

    se agrega migracion para features se crea su controlador y su modelo

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -7,21 +7,25 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-function createOrUpdateCategory (Request $request) {
-    if ($request["idCategory"] == null) {
-        $category = new Category;
-        $category->categoryName = $request["categoryName"];
-        $category->isActive = 1; 
-        $category->status = 1;
-        $respuesta = $category->save();
-        return $respuesta;
+    function createOrUpdateCategory (Request $request) {
+        if ($request["idCategory"] == null) {
+            $category = new Category;
+            $category->categoryName = $request["categoryName"];
+            $category->isActive = 1; 
+            $category->status = 1;
+            $respuesta = $category->save();
+            return $respuesta;
+        }
+        else {
+            $category = Category::find($request["idCategory"]);
+            $category->categoryName = $request["categoryName"];
+            $respuesta = $category->save();
+            return $respuesta;
+        }
+
     }
-    else {
-        $category = Category::find($request["idCategory"]);
-        $category->categoryName = $request["categoryName"];
-        $respuesta = $category->save();
-        return $respuesta;
+    function getCategory (Request $request)  {
+      //se necesita buscar por nombre de categoria  
     }
 
-} 
 }
--- app/Http/Controllers/DishController.php
+++ app/Http/Controllers/DishController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class DishController extends Controller
 {
-    //
+    function createOrUpdateDish (Request $request) {
+        //en features se va colocar el id de los ingredientes que llevara dicho plato. con la finalidad que en el pato aparesca que contiene el plato
+
+    }
+    function getDish (Request $request)  { 
+    }
 }
