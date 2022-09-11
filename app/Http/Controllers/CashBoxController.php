59aa1af2901aebb2cba0d09148215edcedac9610
DunkaN <lguarnizl@gmail.com>
2022-09-11 11:26:00 -0500

    probando subir cambio despues de cambiar el nombre del repositorio

--- app/Http/Controllers/CashBoxController.php
+++ app/Http/Controllers/CashBoxController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 
 class CashBoxController extends Controller
 {
+  //comentario cashbox
     function getAllCashBox ()  {
       $allCategory = CashBox::get();
       return $allCategory;
